10058585f040a077f9feb331dc17378a67f18486
redtail <hongwei0522@gmail.com>
2021-09-29 20:00:46 +0800

    特效

--- page1/part3/all.js
+++ page1/part3/all.js
@@ -22,17 +22,33 @@ window.addEventListener('scroll' , () => {
 });
 
 //settimeout loading畫面 https://medium.com/@AntheaLee/js-%E8%A8%88%E7%AE%97%E7%A7%92%E6%95%B8%E9%9A%B1%E8%97%8F%E7%89%A9%E4%BB%B6-f7b24f7e1a0
-window.setTimeout(loading,1000);
-function loading () {
-  document.getElementById("loading").style.display = "none";
-}
+
+setTimeout(function (){
+  document.getElementById("loading1").style.display = "none";
+},3000);
+setTimeout(function (){
+  document.getElementById("loading2").style.display = "none";
+},3000);
+setTimeout(function (){
+  document.getElementById("frontlogo").style.display = "none";
+},3000);
+
 
 //search 按鈕
-window.onload = function(){
-  document.getElementById("search").onclick = function(){
-    if(document.getElementById("hide").style.display == "none")
-    document.getElementById("hide").style.display = "block";
+var search = document.getElementById('search');
+
+search.addEventListener("click", function(){
+  if(document.getElementById("hide").style.display == "none" && 
+      document.getElementById("hide2").style.display == "none")
+    document.getElementById("hide").style.display = "block" ,
+    document.getElementById("hide2").style.display = "block";
     else
-    document.getElementById("hide").style.display = "none";
-  }
-}
+    document.getElementById("hide").style.display = "none" ,
+    document.getElementById("hide2").style.display = "none";
+})
+
+var hide2 = document.getElementById('hide2');
+hide2.addEventListener('click',function(){
+    document.getElementById("hide").style.display = "none" ,
+    document.getElementById("hide2").style.display = "none";
+})
